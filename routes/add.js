48d2eb68dcf9be2ee69c37d35c88b5676f024f14
anmolm96 <anmol.maini@gmail.com>
2015-02-10 18:56:21 -0800

    Saves new friends to database

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,20 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
+exports.addFriend = function(req, res)
+{ 
 	// Your code goes here
+	res.render('add');
+	var name = req.query.name;
+	var description = req.query.description;
+	var image = "http://lorempixel.com/400/400/people"
+	var newFriend = {"name": name, "description": description, "imageURL": image};
+	console.log(newFriend);
+	data["friends"].push(newFriend);
+	var fs = require('fs');
+	var outputFilename = 'data.json';
+	fs.writeFile(outputFilename,JSON.stringify(data,null,4),function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
  }
